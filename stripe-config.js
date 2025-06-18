8c2d339b944c8866f85da51bfcaca466a20d4e80
AdrianoTorres <adrianotrc@gmail.com>
2025-06-18 19:54:07 -0300

    fix: Adiciona stripe-config.js ao projeto para deploy

---
+++ stripe-config.js
@@ -0,0 +1,5 @@
+// stripe-config.js
+// ATENÇÃO: Substitua o texto abaixo pela sua CHAVE PUBLICÁVEL da Stripe.
+// Ela começa com 'pk_live_...' ou 'pk_test_...'.
+
+export const STRIPE_PUBLISHABLE_KEY = 'pk_live_51RZZhoI6qNljWf7uI7u1uO0yMPRCZmr8tkL8aU9fwCVUq81eEFV02Qemdhli5XXo588cML8s7w4gPNgGyvbrLEkI00H5nPZBDF';
