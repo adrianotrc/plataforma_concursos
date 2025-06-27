ac006f241b9ff2221cc67bfeeb85f1f9be26ae66
AdrianoTorres <adrianotrc@gmail.com>
2025-06-27 20:40:11 -0300

    fix: Melhora script de build para resolver erro de deploy

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@
 # exit on error
 set -o errexit
 
+# Garante que as ferramentas de instalação estejam atualizadas
+pip install --upgrade pip setuptools wheel
+
+# Instala as dependências listadas no requirements.txt
 pip install -r requirements.txt
