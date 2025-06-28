8f53e40d5f4ec9ddbee94237020756cbbee90a62
AdrianoTorres <adrianotrc@gmail.com>
2025-06-27 21:34:31 -0300

    refactor: Simplifica build e consolida start command para Render

--- build.sh
+++ build.sh
@@ -2,8 +2,5 @@
 # exit on error
 set -o errexit
 
-# Garante que as ferramentas de instalação estejam atualizadas
-pip install --upgrade pip setuptools wheel
-
-# Instala as dependências listadas no requirements.txt
-pip install -r requirements.txt
+# Apenas garante que a ferramenta pip esteja disponível
+pip install --upgrade pip
